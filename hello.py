646812128d5fde3beae29f957f0d5288648ba03e
Tanmay Raut <126262361+Tanmayraut16@users.noreply.github.com>
2024-06-19 13:37:31 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,9 @@
+# side = float(input("Enter Side of square : "))
+# print(side * side)
+
+str = "Tanmay Raut"
+print(str[-4 : -6])
+print(str[2 : 2])
+print(str[4 : 10])
+print(str[0 : ])
+print(str[ : 6])
