0940ef246f76ecfec16180d07373bcc686f33545
Tanmay Raut <126262361+Tanmayraut16@users.noreply.github.com>
2024-06-30 22:35:40 +0530

    Add files via upload

---
+++ Lec6.py
@@ -0,0 +1,11 @@
+# Functions in pytthon
+#   Block of Statement that performs a specific task
+#   Syntax -  
+#  def func_name(param 1, param 2) : 
+#       Some work
+#       return val
+
+def cal_Sum(a , b):
+    return a+b
+
+print(cal_Sum(10,20)) 
---
+++ Lec7.py
@@ -0,0 +1,16 @@
+# File Handeling
+# f = open("xyz.txt", "r") #To read a file
+# data = f.read()
+# print(data)
+# print(type(data))
+# f.close() 
+
+
+# f = open("demo.txt" , "r+ ")
+# # f.write("Hi this is a new File")
+# f.write("123454sgsfvdfv")
+# print(f.read())
+# f.close()
+
+import os
+os.remove("xyz.txt")
